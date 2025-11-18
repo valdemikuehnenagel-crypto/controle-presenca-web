31b863997f259d4bad9fee83a61d7eeef70a2eee
valdemikuehnenagel-crypto <valdemi.kuehne.nagel@gmail.com>
2025-11-18 10:24:24 -0300

    fix: Correção importação supabase em separacao.js

--- src/pages/separacao.js
+++ src/pages/separacao.js
@@ -1,6 +1,6 @@
 import {Html5Qrcode, Html5QrcodeSupportedFormats} from 'html5-qrcode';
 import qrcode from 'qrcode-generator';
-import {createClient} from 'https://esm.sh/@supabase/supabase-js@2';
+import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://tzbqdjwgbisntzljwbqp.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6YnFkandnYmlzbnR6bGp3YnFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0MTQyNTUsImV4cCI6MjA3MTk5MDI1NX0.fl0GBdHF_Pc56FSCVkKmCrCQANMVGvQ8sKLDoqK7eAQ';
